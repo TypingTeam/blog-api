cf23cfc40d5b6a350d2bc8f9a85300efdf6828d0
Sergii Lischuk <sergii.lischuk@gmail.com>
2024-07-22 22:14:08 +0300

    Added skeleton project for API

---
+++ src/Keeper.API/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapGet("/", context => context.Response.WriteAsync("Hello from Keeper!"))
+    .WithName("root")
+    .WithOpenApi();
+
+app.Run();
