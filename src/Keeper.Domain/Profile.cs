bfa722325b748b7a08c2d007d9acdaceb42111a4
Sergii Lischuk <evertodante@gmail.com>
2024-07-30 22:37:52 +0300

    Added profile

---
+++ src/Keeper.Domain/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace Keeper.Domain;
+
+public class Profile: Entity
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Position { get; set; }
+    public string Image { get; set; }
+}
