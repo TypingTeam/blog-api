cf21db0fd5a31ee4bc2bf83bd040931a4a34e94c
Sergii Lischuk <evertodante@gmail.com>
2024-08-19 22:47:29 +0300

    Updated profile

--- src/Keeper.Domain/Profile.cs
+++ src/Keeper.Domain/Profile.cs
@@ -2,8 +2,22 @@
 
 public class Profile: Entity
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Position { get; set; }
-    public string Image { get; set; }
+    private Profile() { }
+    
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public string Position { get; private set; }
+    public string Image { get; private set; }
+    public bool isActive { get; private set; }
+
+    public static Profile Create(string name, string surname, string position, string image)
+    {
+        return new Profile() { Name = name, Surname = surname, Position = position, Image = image };
+    }
+
+    public bool Activate()
+    {
+        isActive = true;
+        return isActive;
+    }
 }
